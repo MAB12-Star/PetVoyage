138b04b8da61fda14aa80879800a21e2f40d2847
Michael <michael.aron.blue@gmail.com>
2024-11-11 21:34:35 -0500

    here

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+//const User = require('../models/User');
 const Regulation = require('../models/regulation');
 const { isLoggedIn } = require('../middleware');
 const Airline = require('../models/airline');
