cdbe11c026edb93284c41bb5b93b7325f127fe7c
Michael <michael.aron.blue@gmail.com>
2026-01-17 21:02:55 -0500

    here

--- app.js
+++ app.js
@@ -59,6 +59,11 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.locals.req = req;
+  next();
+});
+
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use(express.json());
