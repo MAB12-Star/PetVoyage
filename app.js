3f6f4f48f7147bc350c8eb4596666f6c6c569149
Michael <michael.aron.blue@gmail.com>
2026-01-24 20:59:36 -0500

    here

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ const { attachAds } = require('./middleware');
 const sitemapRoutes = require('./routes/sitemap');
 const { redirectOldAirlineLinks, toDoListMiddleware } = require('./middleware');
 const e = require('connect-flash');
-const adminAgentRoutes = require("./routes/adminAgent");
+
 
 
 mongoose.connect(process.env.mongoKey, {});
@@ -149,7 +149,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/admin/agent", adminAgentRoutes);
+app.use("/admin", require("./routes/adminAgent"));
 app.use("/admin", adminRoutes);
 
 
